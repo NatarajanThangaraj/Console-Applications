235ad3009cb96a4eb6597ad32378a7ac211bab42
NatarajanThanagaraj <natarajanthanagarajcareer@gmail.com>
2023-12-11 18:26:57 +0530

    update 2.0

--- src/com/natarajanthangaraj/gasrefillbooking/chatbotapplication/verifycustomer/VerifyCustomer.java
+++
@@ -1,5 +0,0 @@
-package com.natarajanthangaraj.gasrefillbooking.chatbotapplication.verifycustomer;
-
-public class VerifyCustomer {
-
-}
--- src/com/natarajanthangaraj/gasrefillbooking/chatbotapplication/verifycustomer/VerifyCustomerViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.natarajanthangaraj.gasrefillbooking.chatbotapplication.verifycustomer;
-
-public class VerifyCustomerViewModel {
-
-	
-
-}
